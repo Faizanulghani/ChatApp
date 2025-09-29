f846e27f4395611aa03f81c3bf9dd16f79ba1c33
Faizan Ul Ghani <faizanulghani1@gmail.com>
2025-09-29 03:36:52 -0700

    Setup React-Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import SignUpPage from "./components/SignUpPage";
+import LoginPage from "./components/LoginPage";
+import SettingPage from "./components/SettingPage";
+import ProfilePage from "./components/ProfilePage";
+import Navbar from "./components/Navbar";
+
 const App = () => {
   return (
     <div>
-    
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/setting" element={<SettingPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
     </div>
   );
 };
