bf6d61f8ad76347a65e18689b3a0428f2316178d
Faizan Ul Ghani <faizanulghani1@gmail.com>
2025-10-01 00:26:06 -0700

    chat store created

---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,37 @@
+import { create } from "zustand";
+import { api } from "../lib/axios";
+import toast from "react-hot-toast";
+
+export const useChatStore = create((set) => ({
+  messages: [],
+  users: [],
+  selectedUser: null,
+  isUsersLoading: false,
+  isMessagesLoading: false,
+
+  getUsers: async () => {
+    set({ isUsersLoading: true });
+    try {
+      const res = await api.get("/message/users");
+      set({ users: res.data });
+    } catch (error) {
+      console.log("Error in getUsers", error);
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isUsersLoading: false });
+    }
+  },
+
+  getMessages: async (userId) => {
+    set({ isMessagesLoading: true });
+    try {
+      let res = await api.get(`/message/${userId}`);
+      set({ messages: res.data });
+    } catch (error) {
+      console.log("Error in getMessages", error);
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isMessagesLoading: false });
+    }
+  },
+}));
