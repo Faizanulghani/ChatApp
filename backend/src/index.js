3308cda469d3465fc94ea080eb24b0edac2cfd85
Faizan Ul Ghani <faizanulghani1@gmail.com>
2025-10-02 05:18:54 -0700

    Project Completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,8 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import path from "path";
+
 import { connectDB } from "./lib/db.js";
 
 import { io, app, server } from "./lib/socket/socket.js";
@@ -12,6 +14,7 @@ import { io, app, server } from "./lib/socket/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
@@ -26,6 +29,14 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -87,7 +87,7 @@ export const useAuthStore = create((set, get) => ({
     const { authUser } = get();
     if (!authUser || get().socket?.connected) return;
 
-    const socket = io("http://localhost:5001", {
+    const socket = io(import.meta.env.MODE  === "development" ? "http://localhost:5001" : "/", {
       query: {
         userId: authUser._id,
       },
