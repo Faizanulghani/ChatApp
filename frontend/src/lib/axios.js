5f75edb2aabeb933b2f18028c1532afe58eb3a21
Faizan Ul Ghani <faizanulghani1@gmail.com>
2025-10-03 00:50:06 -0700

    Added vercel

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const api = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
-      ? "https://chatapp-9ourksjb1-faizan-ul-ghanis-projects.vercel.app/"
+      ? "https://chatapp-nine-woad.vercel.app/"
       : "/api",
   withCredentials: true,
 });
